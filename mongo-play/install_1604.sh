23193df9243749ee48e4b1ab1a6938d20d37d24b
x74feng <xiyangf1997@gmail.com>
2019-12-03 16:47:38 -0500

    add mongo-play

---
+++ mongo-play/install_1604.sh
@@ -0,0 +1,6 @@
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
---
+++ mongo-play/ycsb/install.sh
@@ -0,0 +1,2 @@
+curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
+tar xfvz ycsb-0.17.0.tar.gz
